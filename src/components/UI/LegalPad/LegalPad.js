d2c0dae2079b9b6325f7c5d4e70411cf53f986a6
babyazalea <usun12@hotmail.com>
2021-08-05 10:39:02 +0900

    added legal-pad

---
+++ src/components/UI/LegalPad/LegalPad.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./LegalPad.module.css";
+
+const LegalPad = () => {
+  return <div className={classes["legal-pad"]}></div>;
+};
+
+export default LegalPad;
