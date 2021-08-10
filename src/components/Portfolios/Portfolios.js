319b2bdc3e282e86e684b506b2efec121cd70fd6
babyazalea <usun12@hotmail.com>
2021-08-10 21:32:39 +0900

    added card-component & card-animation in legal-pad

--- src/components/Portfolios/Portfolios.js
+++ src/components/Portfolios/Portfolios.js
@@ -2,15 +2,20 @@ import React from "react";
 
 import Backdrop from "../UI/Backdrop/Backdrop";
 import LegalPad from "../UI/LegalPad/LegalPad";
+import Card from "../UI/Card/Card";
 
 const Portfolios = (props) => {
-  const text = "이것은 포트폴리오입니다.<br/> test text.";
+  // const text = "이것은 포트폴리오입니다.<br/> test text.";
 
   return (
     <React.Fragment>
       <Backdrop onClose={props.viewCloseHandler} />
       <div className="views portfolios">
-        <LegalPad text={text} />
+        <LegalPad>
+          <Card onLegalPad={true}>
+            <p>good</p>
+          </Card>
+        </LegalPad>
       </div>
     </React.Fragment>
   );
--- src/components/UI/LegalPad/LegalPad.js
+++ src/components/UI/LegalPad/LegalPad.js
@@ -1,28 +1,52 @@
 import React from "react";
-import { useSpring, config, animated } from "react-spring";
+import {
+  useSpring,
+  config,
+  animated,
+  useSpringRef,
+  useTransition,
+  useChain,
+} from "react-spring";
 
 import "./LegalPad.css";
 
 const LegalPad = (props) => {
   // min length for list: 20
   let transformedArray;
+  let emptyLines = [];
+
   if (props.text) {
     transformedArray = props.text.split("<br/>");
   }
 
-  let emptyLines = [];
-  if (transformedArray.length < 20) {
+  if (transformedArray && transformedArray.length < 20) {
     const count = 20 - transformedArray.length;
     for (let i = 1; i < count; i++) {
       emptyLines.push("");
     }
+  } else {
+    for (let i = 1; i < 20; i++) {
+      emptyLines.push("");
+    }
   }
 
+  const legalPadSpringApi = useSpringRef();
   const animation = useSpring({
+    ref: legalPadSpringApi,
     to: { height: "100%" },
     from: { height: "0%" },
     config: config.default,
   });
+
+  const cardTransApi = useSpringRef();
+  const transitions = useTransition(true, {
+    ref: cardTransApi,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
+  useChain([legalPadSpringApi, cardTransApi], [0, 0.15]);
 
   return (
     <animated.div style={animation}>
@@ -30,15 +54,20 @@ const LegalPad = (props) => {
         <ul>
           <li></li>
           <li></li>
-          {transformedArray.map((line, index) => (
-            <li key={"text" + index}>
-              <span>{line}</span>
-            </li>
-          ))}
+          {transformedArray
+            ? transformedArray.map((line, index) => (
+                <li key={"text" + index}>
+                  <span>{line}</span>
+                </li>
+              ))
+            : null}
           {emptyLines.map((emptyLine, index) => (
             <li key={"empty" + index}></li>
           ))}
         </ul>
+        {transitions((styles) => (
+          <animated.div style={styles}>{props.children}</animated.div>
+        ))}
       </div>
     </animated.div>
   );
