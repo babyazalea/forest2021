69d68e5803d9b824a90f9ecca3db41cea369a23a
babyazalea <usun12@hotmail.com>
2021-08-11 01:00:10 +0900

    clean up code

--- src/components/Portfolios/Portfolios.js
+++ src/components/Portfolios/Portfolios.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Backdrop from "../UI/Backdrop/Backdrop";
 import LegalPad from "../UI/LegalPad/LegalPad";
-import Card from "../UI/Card/Card";
 
 const Portfolios = (props) => {
   const portfolios = [
@@ -15,17 +14,7 @@ const Portfolios = (props) => {
     <React.Fragment>
       <Backdrop onClose={props.viewCloseHandler} />
       <div className="views portfolios">
-        <LegalPad portfolios={portfolios}>
-          {/* <div className="portfolio-container">
-            <ul>
-              <li>
-                <Card>
-                  <p>good</p>
-                </Card>
-              </li>
-            </ul>
-          </div> */}
-        </LegalPad>
+        <LegalPad portfolios={portfolios} />
       </div>
     </React.Fragment>
   );
