8c42df928b66a091eb4c719d17af6e5d7fc90ca8
babyazalea <usun12@hotmail.com>
2021-08-19 01:35:51 +0900

    added loading-component

---
+++ src/components/UI/LoadingDots/LoadingDots.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./LoadingDots.css";
+
+const LoadingDots = () => {
+  return (
+    <div className="loading__dots">
+      <div className="bounce1"></div>
+      <div className="bounce2"></div>
+      <div className="bounce3"></div>
+    </div>
+  );
+};
+
+export default LoadingDots;
