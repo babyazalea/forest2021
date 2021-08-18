ef14da2e632427be58ffcff93dfe6f3d4472d8ba
babyazalea <usun12@hotmail.com>
2021-08-19 02:25:12 +0900

    storing logged-in in local-storage

--- src/hooks/auth-hooks.js
+++ src/hooks/auth-hooks.js
@@ -4,11 +4,13 @@ export const useAuth = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const login = (responseData) => {
+    localStorage.setItem("isLoggedIn", true);
     localStorage.setItem("localId", responseData["localId"]);
     setIsLoggedIn(true);
   };
 
   const logout = () => {
+    localStorage.removeItem("isLoggedIn");
     localStorage.removeItem("localId");
     setIsLoggedIn(false);
   };
