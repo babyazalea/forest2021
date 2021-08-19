5b91507ba24c500f6dee093ea9ffba5c9e348975
babyazalea <usun12@hotmail.com>
2021-08-19 12:41:06 +0900

    added auto-login

--- src/hooks/auth-hooks.js
+++ src/hooks/auth-hooks.js
@@ -1,19 +1,35 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const useAuth = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const login = (responseData) => {
+    const expirationDate = new Date(
+      new Date().getTime() + responseData["expiresIn"] * 1000
+    );
+
     localStorage.setItem("isLoggedIn", true);
+    localStorage.setItem("expirationDate", expirationDate);
     localStorage.setItem("localId", responseData["localId"]);
     setIsLoggedIn(true);
   };
 
-  const logout = () => {
+  const logout = useCallback(() => {
     localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("expirationDate");
     localStorage.removeItem("localId");
     setIsLoggedIn(false);
-  };
+  }, []);
+
+  useEffect(() => {
+    const expDate = new Date(localStorage.getItem("expirationDate"));
+
+    if (!expDate || expDate <= new Date()) {
+      logout();
+    } else {
+      setIsLoggedIn(true);
+    }
+  }, [logout]);
 
   return {
     isLoggedIn,
